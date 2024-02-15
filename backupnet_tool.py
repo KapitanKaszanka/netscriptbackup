3661919362fdb2208fb9425425bb4864492a3a49
Mateusz Krupczyński <mateuszkrupczynski@gmail.com>
2024-02-15 10:34:23 +0100

    Added more git validation

--- backupnet_tool.py
+++ backupnet_tool.py
@@ -8,13 +8,16 @@ from pathlib import Path
 from datetime import datetime
 
 
+
 CONFIG_LOADED = Config_Load()
 LOGGER = CONFIG_LOADED.set_logging()
 
 
 
 class Backup():
-
+    """
+    Backup object.
+    """
 
     def __init__(self) -> None:
         self.logger = logging.getLogger("backup_app.Backup")
@@ -24,54 +27,30 @@ class Backup():
         self.configs_path = CONFIG_LOADED.configs_path
 
 
-    def get_configuration(self):
-        for dev in Device.devices_lst:
-            self.logger.info(f"Start creating backup for: {dev.ip}")
-            ssh = SSH_Connection(dev)
-            stdout = ssh.get_config()
-
-            if isinstance(stdout, str):
-                self.logger.info(f"Writing config to file and execute git commit {dev.ip}")
-                self.logger.debug("Writing config to file.")
-                path = self.configs_path 
-                done = self.write_config(path, dev.ip, dev.name, stdout)
-
-                if done:
-                    self.logger.debug(f"Git commands execute {dev.ip}")
-                    done = self.git_execute(path, dev.ip, dev.name)
-                    if not done:
-                        self.logger.error(f"Can't create backup config {dev.ip}")
-                        pass
-                else:
-                    self.logger.error(f"Can't create backup config dev.ip")
-                    pass
-            else:
-                self.logger.warning(f"Can't connect to device {dev.ip}")
-                pass
-
-
-    def write_config(self, path, ip, name, stdout):
+    def _file_working(self, ip, name, stdout):
         try:
-            file_path = f"{path}/{name}_{ip}/"
-            path = Path(file_path)
+            dir_path = f"{self.configs_path}/{name}_{ip}"
+            file_path = f"{self.configs_path}/{name}_{ip}/{ip}_conf.txt"
 
-            self.logger.debug(f"Check if folder {file_path} exist.")
+            path = Path(dir_path)
+
+            self.logger.debug(f"Check if folder {dir_path} exist.")
 
             if not path.is_dir():
                 self.logger.debug(
-                    f"Folder {file_path} don't exist or account don't have permionss. Creating."
+                    f"Folder {dir_path}/ don't exist or account don't have permionss. Creating."
                     )
                 path.mkdir()
 
             try:
-                self.logger.debug(f"Opening file: {file_path}config.txt")
-                with open(f"{file_path}config.txt", "w") as f:
+                self.logger.debug(f"Opening file: {file_path}")
+                with open(file_path, "w") as f:
                     self.logger.debug(f"Writing config for {ip}")
                     f.writelines(stdout)
                 return True
 
             except PermissionError:
-                self.logger.warning(f"Can't open {file_path}/config.txt. Permission error.")
+                self.logger.warning(f"Can't open {file_path}. Permission error.")
                 return False
 
         except Exception as e:
@@ -79,40 +58,64 @@ class Backup():
             pass
 
 
-    def git_execute(self, path, ip, name):
-        file_path = f"{path}/{name}_{ip}/"
-        self.logger.debug(f"Check if git repozitory exist in {file_path}.")
-        git_path = Path(f"{file_path}.git").is_dir()
+    def _git_working(self, ip, name):
+        dir_path = f"{self.configs_path}/{name}_{ip}"
+        file_name = f"{ip}_conf.txt"
+
+        self.logger.debug(f"Check if git repozitory exist in {dir_path}.")
+        git_path = Path(f"{dir_path}.git").is_dir()
 
         if not git_path:
             try:
-                self.logger.debug(f"Repository don't exist in {file_path}. Creating")
+                self.logger.debug(f"Repository don't exist in {dir_path}. Creating")
                 cmd = subprocess.Popen([
                     "/usr/bin/git", "init"],
-                    cwd = file_path,
+                    cwd = dir_path,
                     stdout = subprocess.DEVNULL
                     )
-                cmd.communicate()
-                
-                self.logger.debug(f"Adding all file to repozitory in {file_path}")
+                output = cmd.communicate()
+                _string_output = output[0].decode()
+
+                if "Initialized" not in _string_output:
+                    self.logger.info(f"Can't initialize git in {dir_path}")
+                    return False
+
+                self.logger.debug(f"Adding all file to repozitory in {git_path}")
                 cmd = subprocess.Popen(
                     ["/usr/bin/git", "add", "-A"],
-                    cwd = file_path,
+                    cwd = dir_path,
                     stdout = subprocess.DEVNULL
                     )
-                cmd.communicate()
+                
+                self.logger.debug(f"Checking status for {git_path}")
+                cmd = subprocess.Popen(
+                    ["/usr/bin/git", "status"],
+                    cwd = dir_path,
+                    stdout = subprocess.DEVNULL
+                    )
+                
+                output = cmd.communicate()
+                _string_output = output[0].decode()
+                
+                for line in _string_output.splitlines():
+                    if "new file:" in line and {file_name} in line:
+                        self.logger.debug(f"{file_name} added to git repozitory.")
+
+                    else:
+                        self.logger.warning(f"{file_name} has not been added to the repository.")
+                        return False
 
             except Exception as e:
                 self.logger.error(f"Error ocure: {e}")
                 return False
         else:
-            self.logger.debug(f"Repository exist in {file_path}.")
+            self.logger.debug(f"Repository exist in {dir_path}.")
 
         try:
-            self.logger.info(f"Commiting repository {file_path}")
+            self.logger.info(f"Commiting repository {dir_path}")
             cmd = subprocess.Popen(
                 ["/usr/bin/git", "commit", "-am", f"{datetime.now().date()}-{datetime.now().time()}"],
-                cwd = file_path,
+                cwd = dir_path,
                 stdout = subprocess.PIPE
                     )
 
@@ -121,10 +124,12 @@ class Backup():
 
             if "nothing to commit" in _string_output:
                 self.logger.info(f"Nothing to commit for {ip}")
+
             elif "file changed" in _string_output and "rewrite" in _string_output:
                 self.logger.info(f"Config change for {ip}")
+
             elif "Untracked files" in _string_output:
-                self.logger.warning(f"Untracked files in {file_path}")
+                self.logger.warning(f"Untracked files in {dir_path}")
 
             return True
 
@@ -133,11 +138,38 @@ class Backup():
             return False
 
 
+    def execute_backup(self):
+        for dev in Device.devices_lst:
+            self.logger.info(f"Start creating backup for: {dev.ip}")
+            ssh = SSH_Connection(dev)
+            stdout = ssh.get_config()
+
+            if isinstance(stdout, str):
+                self.logger.info(f"Writing config to file and execute git commit {dev.ip}")
+                self.logger.debug("Writing config to file.")
+                done = self._file_working(dev.ip, dev.name, stdout)
+
+                if done:
+                    self.logger.debug(f"Git commands execute {dev.ip}")
+                    done = self._git_working(dev.ip, dev.name)
+                    if not done:
+                        self.logger.error(f"Can't create backup config {dev.ip}")
+                        pass
+
+                else:
+                    self.logger.error(f"Can't create backup config {dev.ip}")
+                    pass
+
+            else:
+                self.logger.warning(f"Can't connect to device {dev.ip}")
+                pass
+
+
 
 def backup_execute():
 
     data = Backup()
-    data.get_configuration()
+    data.execute_backup()
 
     return True
 

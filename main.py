ae364a6d09008a1548c3e2788d5c89d9187ded52
Mateusz Krupczyński <mateuszkrupczynski@gmail.com>
2024-02-16 11:10:05 +0100

    Clearing

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 
 from backup_tool import backup_execute
-import time
+
+
 
 def main():
-    
-    start = time.time()
+
     backup_execute()
-    stop = time.time()
-    
-    print(f"Script execute in: {round(stop - start, 2)}")
+
 
 if __name__ == "__main__":
     main()

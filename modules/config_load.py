dda3fad4cb6d6b7283039a08b5595b0a5be6b0ba
Mateusz Krupczyński <mateuszkrupczynski@gmail.com>
2024-02-28 10:46:35 +0100

    c

--- modules/config_load.py
+++ modules/config_load.py
@@ -121,15 +121,11 @@ class Config_Load:
             file_handler.setLevel(logging.ERROR)
         elif self.logging_level.lower() == "critical":
             file_handler.setLevel(logging.CRITICAL)
-        stream_handler = logging.StreamHandler()
-        stream_handler.setLevel(logging.ERROR)
         formatter = logging.Formatter(
             "%(asctime)s:%(name)s:%(levelname)s:%(message)s"
             )
         file_handler.setFormatter(formatter)
-        stream_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
-        logger.addHandler(stream_handler)
         return logger
 
 
